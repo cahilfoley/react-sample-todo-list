261b1fed212d84fa7ba5d62a081795bb750c6ad0
Cahil Foley <cahilfoley2@gmail.com>
2018-09-04 16:59:29 +0800

    Added ability to flag todos as completed

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import Task from './Task'
 
-const TaskList = props => (
-  <div className={props.tasks.length ? '' : 'todo__empty_task_list'}>
-    {props.tasks.map((task, i) => (
-      <Task task={task} key={i} remove={() => props.removeTask(i)} />
-    ))}
-  </div>
-)
+const TaskList = props => {
+  return (
+    <div className={props.tasks.length ? '' : 'todo__empty_task_list'}>
+      {props.tasks.map((task, i) => {
+        return (
+          <Task
+            task={task}
+            key={i}
+            toggle={props.toggleTask(i)}
+            remove={props.removeTask(i)}
+          />
+        )
+      })}
+    </div>
+  )
+}
 
 export default TaskList
